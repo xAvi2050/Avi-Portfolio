0b0fe333bf9e51f458aa81a02bed12a4044c133a
xAvi2050 <cheetahmindset7@gmail.com>
2025-09-03 19:49:35 +0530

    add updated resume 2.0

--- src/utils/ResumeWindow.jsx
+++ src/utils/ResumeWindow.jsx
@@ -22,7 +22,7 @@ const ResumeWindow = ({ onClose }) => {
 
           <div className="flex flex-col gap-3">
             <a
-              href="https://drive.google.com/file/d/1t4HfwNkHvMt3_u45kFzWNXZoxQ0mkQZJ/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1zGw4FgR7D2e67MtKpItmderoTo1h-yxv/view?usp=drive_link"
               target="_blank"
               rel="noopener noreferrer"
               className="bg-main-blue text-white px-4 py-2 text-sm md:text-base rounded-full transition cursor-pointer hover:bg-main-blue/80"
